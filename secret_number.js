5d9ec41baff496332435b4eb1bf39069d1339ab1
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-03-14 19:08:22 -1000

    all tests passed

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,21 @@
-'use strict';
+'use strict'
 module.exports = function() {
-  
+
+  //private
+  var repeatNumber = createRandom();
+
+  function createRandom(){
+    return Math.floor((Math.random() * 1000001));
+  }
+
+  //public
+  function getRepeatNumber(){
+    console.log(repeatNumber);
+    return repeatNumber;
+  }
+
+return getRepeatNumber;
+
 };
+
+
